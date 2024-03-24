ee77cde3fa08cfd730655f4189dfe6fa85f36ad3
Oussema <oussama.chercheri@tek-up.de>
2024-03-24 02:37:49 +0000

    create signup api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 
 dotenv.config();
@@ -15,8 +16,11 @@ console.log('database connected')})
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
