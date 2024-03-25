7fa7d99cbec2c076ac59737d5b6d78977923e697
Oussema <oussama.chercheri@tek-up.de>
2024-03-25 17:09:55 +0000

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Footer from './components/Footer';
 import Header from './components/Header';
 import About from './pages/About';
 import Home from './pages/Home';
@@ -19,5 +20,6 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )}
