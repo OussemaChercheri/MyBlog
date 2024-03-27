95e5bc632ba4ed34f1c3abc31b3fa12e6625fd26
Oussema <oussama.chercheri@tek-up.de>
2024-03-27 05:10:40 +0000

    create api route

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,77 +3,77 @@ import { BsFacebook, BsGithub, BsInstagram, BsLinkedin } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 export default function FooterCom() {
-  return (
+    return (
     <Footer container className='border border-t-8 border-teal-500'>
-      <div className='w-full max-w-7xl mx-auto'>
+        <div className='w-full max-w-7xl mx-auto'>
         <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
-          <div className='mt-5'>
+            <div className='mt-5'>
             <Link
-              to='/'
-              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+                to='/'
+                className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
-              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
                 Oussema`s Blog
-              </span>
+                </span>
             </Link>
-          </div>
-          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
-            <div>
-              <Footer.Title title="About" />
-              <Footer.LinkGroup col>
-                <Footer.Link
-                  href="https://oussemachercheri.github.io/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  My Portfolio
+            </div>
+            <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+                <div>
+                <Footer.Title title="About" />
+                <Footer.LinkGroup col>
+                    <Footer.Link
+                    href="https://oussemachercheri.github.io/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        My Portfolio
                 </Footer.Link>
                 <Footer.Link
-                  href='/about'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                    href='/about'
+                    target='_blank'
+                    rel='noopener noreferrer'
                 >
-                  Oussema`s Blog
+                    Oussema`s Blog
                 </Footer.Link>
-              </Footer.LinkGroup>
+                </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title='Follow us' />
-              <Footer.LinkGroup col>
+                <Footer.Title title='Follow us' />
+                <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://github.com/OussemaChercheri'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                    href='https://github.com/OussemaChercheri'
+                    target='_blank'
+                    rel='noopener noreferrer'
                 >
-                  Github
+                    Github
                 </Footer.Link>
                 <Footer.Link href='https://discord.gg/nRrWx6m9'>Discord</Footer.Link>
-              </Footer.LinkGroup>
+                </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title='Legal' />
-              <Footer.LinkGroup col>
+                <Footer.Title title='Legal' />
+                <Footer.LinkGroup col>
                 <Footer.Link href='#'>Privacy Policy</Footer.Link>
                 <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
-              </Footer.LinkGroup>
+                </Footer.LinkGroup>
             </div>
-          </div>
+            </div>
         </div>
         <Footer.Divider />
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
-          <Footer.Copyright
+            <Footer.Copyright
             href='#'
             by="Sahand's blog"
             year={new Date().getFullYear()}
-          />
-          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            />
+            <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
             <Footer.Icon href='https://www.facebook.com/oussema.chercheri.9/' icon={BsFacebook}/>
             <Footer.Icon href='https://www.instagram.com/chercherioussema/' icon={BsInstagram}/>
             <Footer.Icon href='https://www.linkedin.com/in/chercheri-oussema-807213218/' icon={BsLinkedin}/>
             <Footer.Icon href='https://github.com/OussemaChercheri' icon={BsGithub}/>
-          </div>
+            </div>
         </div>
-      </div>
+        </div>
     </Footer>
-  );
+    );
 }
