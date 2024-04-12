e8104bc1862d207cf363aed4d95e85db37bfd7e3
Oussema <oussama.chercheri@tek-up.de>
2024-04-12 21:49:23 +0000

    add profile UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,14 +1,29 @@
 
+import { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 export default function DashProfile() {
-    const {currentUser} = useSelector(state => state.user)
+    const {currentUser} = useSelector(state => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const filePickerRef = useRef();
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+        }
+        setImageFile(e.target.files[0]);
+
+    };
+    console.log(imageFile, imageFileUrl);
     return (
-        <div className='max-w-lg mx-auto p-6 w-full rounded-lg shadow-md'>
-    <h1 className='my-7 text-center font-semibold text-3xl text-gray-800'>Profile</h1>
-    <form action="#" className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full mx-auto">
-            <img src={currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-4 border-gray-200' />
+    <div className='max-w-lg mx-auto p-6 w-full rounded-lg shadow-md'>
+        <h1 className='my-7 text-center font-semibold text-3xl text-gray-800'>Profile</h1>
+        <form action="#" className="flex flex-col gap-4">
+        <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef}/>
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full mx-auto" onClick={()=> filePickerRef.current.click()}>
+            <img src={imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-4 border-gray-200' />
         </div>
         <div className="flex flex-col gap-2">
             <label htmlFor="username" className="text-gray-600">Username</label>
