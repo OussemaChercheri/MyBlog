f80ba01886d201ebb0716eaba94c7753c1206244
Oussema Chercheri <oussama.chercheri@tek-up.de>
2024-05-26 16:29:48 +0100

    add update user api route

--- api/index.js
+++ api/index.js
@@ -1,37 +1,39 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import authRoutes from './routes/auth.route.js';
-import userRoutes from './routes/user.route.js';
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then( ()=>{
-
-console.log('database connected')})
-.catch((err) => { console.error(err);});
-
-
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3001, () => {
-    console.log('Server is running on port 3001');
+  console.log("Server is running on port 3001");
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
-app.use((err, req, res, next) =>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Eroor';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
-    
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Eroor";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
